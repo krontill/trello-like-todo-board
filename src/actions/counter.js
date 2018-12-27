6edbbef524636c1ee92ba0c93c07a133256d8879
s.shcherba <s.shcherba@itransition.com>
2018-12-27 19:32:24 +0300

    Add redux and counter.

---
+++ src/actions/counter.js
@@ -0,0 +1,4 @@
+import { INCREMENT, DECREMENT } from '../constants';
+
+export const decrement = () => ({ type: DECREMENT });
+export const increment = () => ({ type: INCREMENT });
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,6 +1,47 @@
 import React from 'react';
 import './content.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { increment, decrement } from '../../actions/counter';
 
-const content = () => <div className="content">content</div>;
+const content = props => {
+  const { count, handleIncrement, handleDecrement } = props;
+  return (
+    <div className="content">
+      <p>
+        Count:
+        {count}
+      </p>
+      <p>
+        <button type="button" onClick={handleIncrement}>
+          Increment
+        </button>
+      </p>
+      <p>
+        <button type="button" onClick={handleDecrement}>
+          Decrementing
+        </button>
+      </p>
+    </div>
+  );
+};
 
-export default content;
+content.propTypes = {
+  count: PropTypes.number.isRequired,
+  handleIncrement: PropTypes.func.isRequired,
+  handleDecrement: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ counter }) => ({
+  count: counter.count,
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleIncrement: () => dispatch(increment()),
+  handleDecrement: () => dispatch(decrement()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(content);
