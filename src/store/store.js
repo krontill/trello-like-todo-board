88fa825be822578aaff0454961cf26e0d944e7d1
s.shcherba <s.shcherba@itransition.com>
2019-01-11 15:58:11 +0300

    Save state in localStorage.

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-underscore-dangle */
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
+
+const localStorageMiddleware = ({ getState }) => next => action => {
+  const result = next(action);
+  localStorage.setItem('applicationState', JSON.stringify(getState()));
+  return result;
+};
 
 const initialState = {};
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = compose(
+  applyMiddleware(localStorageMiddleware)(createStore)(
+    rootReducer,
+    JSON.parse(localStorage.getItem('applicationState')) || initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
