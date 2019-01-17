c0672af8f7b5e641dcb945e57979fe074a5d37a1
s.shcherba <s.shcherba@itransition.com>
2019-01-17 18:41:11 +0300

    Add tests for containers with initial state for renders without crashing.

--- src/container/EditCardModal/index.test.jsx
+++ src/container/EditCardModal/index.test.jsx
@@ -31,3 +31,32 @@ it('EditCardModal renders without crashing', () => {
   );
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('EditCardModal with initial state renders without crashing', () => {
+  const div = document.createElement('div');
+
+  const title =
+    'As React is just a lib, it doesn’t dictate rules about how you should organize and structure your projects. This is nice, because it gives us freedom to try different approaches and adapt the ones that better fit for us. On the other hand, this could cause some confusion for devs that are starting in React world. As React is just a lib, it doesn’t dictate rules about how you should organize and structure your projects. This is nice, because it gives us freedom to try different approaches and adapt the ones that better fit for us. On the other hand, this could cause some confusion for devs that are starting in React world.';
+  const text = 'Text Card';
+  const priority = 'low';
+  const dueDate = '2019-01-02';
+  const labels = ['Green', 'Red', 'Blue'];
+  const id = '095390ad-006f-4fe4-a6ea-63e1ea2e74b9';
+
+  const card = {
+    id,
+    title,
+    text,
+    priority,
+    dueDate,
+    labels,
+  };
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <EditCardModal listId={id} card={card} />
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
